e8fdf73515dd66b9670ae63297b4a59f50460cbc
JS <51195305+sombra2@users.noreply.github.com>
2022-09-05 11:26:51 +0100

    addition of a start message and minor changes in formats and emojis

--- pinger.py
+++ pinger.py
@@ -2,6 +2,9 @@ import credentials # create a separate credentials.py file to store the token, c
 import os
 import requests
 import time
+import datetime
+
+now = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
 
 hostname = credentials.host
 
@@ -13,22 +16,23 @@ def telegram_bot_sendtext(bot_message):
 
 
 while True:
+  telegram_bot_sendtext('📡 Monitoring of host *{}* started.\nTimestamp {}'.format(hostname, now))
   response = os.system('ping -c 1 ' + hostname)
   if response == 0:
     time.sleep(60)
     continue
   else:
     telegram_bot_sendtext(
-      '🔴 *Host ' + hostname + ' is down!* Will check again in 5 minutes.')
-    time.sleep(300)
+      '🔴 *Host ' + hostname + ' is down!* Will check again in 10 minutes.')
+    time.sleep(600)
     response = os.system('ping -c 1 ' + hostname)
     # TODO: the script should continue pinging even when the host is down but without sending any messages to telegram
     if response == 0:
       telegram_bot_sendtext(
-        '🟢 *Host ' + hostname + ' is back up again!* Connection re-established')
+        '🟢 *Host {} is back up again!* Connection re-established\nTimestamp {}'.format(hostname, now))
       continue
     else:
       telegram_bot_sendtext(
-        '🔴 *REMINDER - Host ' + hostname + ' is still down!* Please check connection. This script will now end - please make sure you reboot it once connection is re-established')
+        '🔴 *REMINDER - Host {} is still down!* Please check connection. This script will now end - please make sure you reboot it once connection is re-established\n Timestamp {}'.format(hostname, now))
       break
 
